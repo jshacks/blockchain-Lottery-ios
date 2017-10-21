a91a341bd4418a47e64bb7276f422d2f634e47a5
Serban Chiricescu <serban.chiricescu@qualitance.com>
2017-10-21 13:08:33 +0300

    add user class

---
+++ Lottery/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Lottery
+//
+//  Created by serban chiricescu on 10/21/17.
+//  Copyright © 2017 ATeam. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
