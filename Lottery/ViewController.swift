3c2297270fbc235928bf950a1a109623eee72657
Cosmin Cucu <cucu.cosmin@me.com>
2017-10-21 12:13:47 +0300

    Initial Commit

---
+++ Lottery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lottery
+//
+//  Created by Cosmin Cucu on 10/21/17.
+//  Copyright © 2017 ATeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
