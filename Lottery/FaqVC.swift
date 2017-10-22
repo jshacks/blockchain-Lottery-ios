33ce2a38a11b33f0708c86246130ac8e0cfa5a1a
Cosmin Cucu <cucu.cosmin@me.com>
2017-10-22 13:02:34 +0300

    faq vc

---
+++ Lottery/FaqVC.swift
@@ -0,0 +1,17 @@
+//
+//  FaqVC.swift
+//  Lottery
+//
+//  Created by Cosmin Cucu on 10/22/17.
+//  Copyright © 2017 ATeam. All rights reserved.
+//
+
+import UIKit
+
+class FaqVC: UIViewController{
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        get{
+            return .lightContent
+        }
+    }
+}
